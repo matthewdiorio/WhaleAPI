68d9d49e08639438cd93d5960f95e524dd396519
Matthew Diorio <matthewdiorio@icloud.com>
2021-02-22 23:23:04 -0500

    DOM manipulation

--- Backend/server.js
+++ Backend/server.js
@@ -15,3 +15,9 @@ const factsRouter = require('./routes/facts')
 app.use('/whalesharkfacts', factsRouter)
 
 app.listen(3000, () => console.log('Server has started'))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,83 +1,31 @@
-$(document).ready(function(){
-    	var menu = $('.navbar');
-    	$(window).bind('scroll', function(e){
-    		if($(window).scrollTop() > 140){
-    			if(!menu.hasClass('open')){
-    				menu.addClass('open');
-    			}
-    		}else{
-    			if(menu.hasClass('open')){
-    				menu.removeClass('open');
-    			}
-    		}
-    	});
-  
-  
 
-  $(".scroll").click(function(event){		
-  	event.preventDefault();
-  	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-  	
-  });
-  
-  wow = new WOW(
-        {
-          boxClass:     'wow',      // animated element css class (default is wow)
-          animateClass: 'animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       false        // trigger animations on mobile devices (true is default)
-        }
-      );
-      wow.init();
+wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true        // trigger animations on mobile devices (true is default)
+  }
+);
+wow.init();
 
+getNewFact();
 
-	
+function getNewFact() {
+  axios('http://localhost:3000/whalesharkfacts/random')
+
+  .then(function (response) {
+
+    const whale = response.data;
+    document.getElementById('fact').innerHTML = whale[0].fact;
+    document.getElementById('source').innerHTML = whale[0].source;
+
+  })
+  .catch(function (error){ //error handler
+    
+    console.log("Error: " + error); //prints out the error to the console
 });
+}
 
 
-$(function() {
-  $('#produceplace').click(function() {
-    $('#produceplace-info').fadeToggle();
-    $('#cover').fadeToggle();
-    $('body').off('scroll mousewheel touchmove');
-    disableScroll();
-  })
-  $('#dragonwater').click(function() {
-    $('#dragonwater-info').fadeToggle();
-    $('#cover').fadeToggle();
-    $('body').off('scroll mousewheel touchmove');
-    disableScroll();
-  })
-  $('.close-btn').click(function(){
-    $('#produceplace-info').fadeOut();
-    $('#dragonwater-info').fadeOut();
-    $('#cover').fadeOut();
-    enableScroll();
-  })
-  $(document).mouseup(function (e) {
-    var container = $("#produceplace-info");
-    var cover = $("#cover")
-    if (!container.is(e.target) 
-        && container.has(e.target).length === 0) 
-    {
-        container.fadeOut();
-        cover.fadeOut();
-        enableScroll();
-    }
-  });
-  
-});
 
-
-function disableScroll() { 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
-  
-        window.onscroll = function() { 
-            window.scrollTo(scrollLeft, scrollTop); 
-        }; 
-} 
-  
-function enableScroll() { 
-    window.onscroll = function() {}; 
-} 
