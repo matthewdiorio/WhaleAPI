8a13a6cd7b429aa51d23b2ae69b0b77e9f6bebf5
Matthew Diorio <matthewdiorio@icloud.com>
2021-03-11 23:51:55 -0500

    Routed index.html

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use('/whalesharkfacts', factsRouter)
 
 app.listen(3000, () => console.log('Server has started'))
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client", "index.html"));
+});
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
